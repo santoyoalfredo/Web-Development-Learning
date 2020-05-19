7ec5a5d27cedb6894af416257b79ca1d6c5ad3ef
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-05-19 17:36:34 -0400

    Added pictures and sounds for each button

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,6 +1,38 @@
 var buttons = document.querySelectorAll(".drum");
-buttons.forEach( function () { addEventListener("click", handleClick)} );
 
-function handleClick() {
-    alert("Got clicked!");
+for(i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        switch (this.innerHTML) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "l":
+                var kickBass = new Audio("sounds/kick-bass.mp3");
+                kickBass.play();
+                break;
+            default:
+                break;
+        }
+    })
 }
