43fb1ca43d3e03fff9fc9813a437b2af40d6e340
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-05-20 14:13:55 -0400

    Added press animation and new game logic

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,21 +1,34 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var level = 0;
 var userClickedPattern = [];
 
+$("body").on("keydown", function () {
+    nextSequence();
+})
+
+$(".btn").on("click", function () {
+    var userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+})
+
 function nextSequence() {
+    level++;
+    $("h1").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100, playSound(randomChosenColor));
 }
 
-$(".btn").on("click", function () {
-    var userChosenColor = this.id;
-    userClickedPattern.push(userChosenColor);
-    playSound(userChosenColor);
-})
-
 function playSound(name) {
     var sound = new Audio(src = "sounds/" + name + ".mp3");
     sound.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout( function() { $("#" + currentColor).removeClass("pressed") }, 100);
 }
