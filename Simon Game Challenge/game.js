2b1253cbdc10910f894b0bc2e8aeda9520f3ccb9
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-05-20 13:49:01 -0400

    Separated sound playing into its own function

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -6,13 +6,16 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    $("#" + randomChosenColor).fadeOut(100).fadeIn(100, function () {
-        var sound = new Audio(src="sounds/" + randomChosenColor + ".mp3");
-        sound.play();
-    });
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100, playSound(randomChosenColor));
 }
 
 $(".btn").on("click", function () {
     var userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
 })
+
+function playSound(name) {
+    var sound = new Audio(src = "sounds/" + name + ".mp3");
+    sound.play();
+}
