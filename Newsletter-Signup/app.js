d417bbee5c3579613b0e5e3212f344ed4daa4e3c
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-06-08 12:11:02 -0400

    API POST Project Complete

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,11 +12,53 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-    var firstName = req.body.first;
-    var lastName = req.body.last;
-    var email = req.body.email;
+    const firstName = req.body.first;
+    const lastName = req.body.last;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://api.url.com'
+    const options = {
+        method: 'POST',
+        auth: 'servbot:apikey'
+    }
+
+    const request = https.request(url, options, function(response) {
+        
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html');
+        }
+        else {
+            res.sendFile(__dirname + '/failure.html')
+        }
+
+        response.on('data', function(data) {
+            console.log(JSON.parse(data))
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 })
 
-app.listen(3000, function() {
+app.post('/failure', function(req, res) {
+    res.redirect('/');
+})
+
+app.listen(process.env.PORT || 3000, function() {
     console.log('Server is running on port 3000');
 } );
