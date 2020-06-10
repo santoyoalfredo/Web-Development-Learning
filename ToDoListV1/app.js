65c505e416b6207b1baf4da9018cb41e8eddd806
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-06-10 14:49:07 -0400

    Complete To-Do List v1
    Use layouts and modules

--- ToDoListV1/app.js
+++ ToDoListV1/app.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-Parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
-var items = [];
+const items = [];
+const workItems = [];
 
 app.get('/', function(req, res) {
-    var today = new Date();
-    var currentDay = '';
+    const currentDay = date.getDate();
+    res.render('list', { listTitle: currentDay, list: items});
+})
 
-    var options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
-
-    currentDay = today.toLocaleDateString('en-US', options);
-
-    res.render('list', { day: currentDay, list:items});
+app.get('/work', function(req, res) {
+    res.render('list', { listTitle: 'Work List', list: workItems})
 })
 
 app.post('/', function(req, res) {
-    items.push(req.body.newItem);
-    res.redirect('/');
+
+    console.log(req.body.list);
+    if(req.body.list === 'Work List') {
+        workItems.push(req.body.newItem);
+        res.redirect('/work');
+    }
+    else {
+        items.push(req.body.newItem);
+        res.redirect('/');
+    }
 })
 
 app.listen(3000, function() {
