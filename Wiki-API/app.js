4a6743132d024e81fccb97d1abf1132d5f2d8bf0
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-08-17 11:33:23 -0400

    Add PUT and PATCH request

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -20,47 +20,72 @@ const Article = mongoose.model("Article", articleSchema);
 
 app.route("/articles")
 
-.get(function(req, res) {
-    Article.find(function (err, foundArticles) {
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
-    });
-})
-.post(function (req, res) {
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function (req, res) {
 
-    const article = new Article({ title: req.body.title, content: req.body.content });
-    article.save(function (err) {
-        if (!err) {
-            res.send("Success.");
-        } else {
-            res.send(err);
-        }
+        const article = new Article({ title: req.body.title, content: req.body.content });
+        article.save(function (err) {
+            if (!err) {
+                res.send("Success.");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Success.");
+            } else {
+                res.send(err);
+            }
+        });
     });
-})
-.delete(function (req, res) {
-    Article.deleteMany(function (err) {
-        if (!err) {
-            res.send("Success.");
-        } else {
-            res.send(err);
-        }
-    });
-});
 
 app.route("/articles/:articleTitle")
 
-.get(function(req, res) {
-    Article.findOne({ title: req.params.articleTitle}, function(err, foundArticle) {
-        if(!err) {
-            res.send(foundArticle);
-        } else {
-            res.send(err);
-        }
+    .get(function (req, res) {
+        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticle) {
+            if (!err) {
+                res.send(foundArticle);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .put(function (req, res) {
+        Article.update(
+            { title: req.params.articleTitle },
+            { title: req.body.title, content: req.body.content },
+            { overwrite: true },
+            function (err, results) {
+                if (!err) {
+                    res.send(results);
+                } else {
+                    res.send(err);
+                }
+            });
+    })
+    .patch(function (req, res) {
+        Article.update(
+            { title: req.params.articleTitle },
+            { $set: req.body },
+            function (err) {
+                if (!err) {
+                    res.send("Success.");
+                } else {
+                    res.send(err);
+                }
+            });
     });
-});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
