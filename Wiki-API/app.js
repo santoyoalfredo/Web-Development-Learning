266465efb0ba11b384909b90fa1cbd552a9a33c9
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-08-17 10:35:50 -0400

    Add POST Request

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const e = require("express");
 
 const app = express();
 
@@ -26,6 +27,18 @@ app.get("/articles", function(req, res) {
         }
     });
 });
+
+app.post("/articles", function(req, res) {
+    
+    const article = new Article({ title: req.body.title, content: req.body.content });
+    article.save(function(err) {
+        if(!err) {
+            res.send("Success.");
+        } else {
+            res.send(err);
+        }
+    });
+})
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
