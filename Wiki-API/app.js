8e3e7a9e3cb669ab8f389f0ebc63ee74123585b2
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-08-17 10:42:56 -0400

    Add DELETE request

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -40,6 +40,17 @@ app.post("/articles", function(req, res) {
     });
 })
 
+app.delete("/articles", function(req, res) {
+
+    Article.deleteMany(function(err) {
+        if(!err) {
+            res.send("Success.");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
