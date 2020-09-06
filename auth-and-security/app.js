c2ef36641f6bf328a51fa558a63336a170acb1e2
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-09-06 14:59:52 -0400

    Complete project

--- auth-and-security/app.js
+++ auth-and-security/app.js
@@ -30,7 +30,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleID: String
+    googleID: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -82,12 +83,36 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+    User.find({"secret":{$ne:null}}, function(err, foundUsers) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+    })
+});
+
+app.get("/submit", function(req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.render("login");
     }
 });
+
+app.post("/submit", function(req, res) {
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function(err, foundUser) {
+        if(err) {
+            console.log(err);
+        } else {
+            foundUser.secret = submittedSecret;
+            foundUser.save();
+            res.redirect("/secret");
+        }
+    })
+})
 
 app.get("/logout", function (req, res) {
     req.logout();
