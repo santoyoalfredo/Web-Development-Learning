117229371a87b0b5b8ba447ec3f5ead52f7963ce
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-09-08 13:10:58 -0400

    Complete Part 1

---
+++ Keeper App/src/components/note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return(
+        <div className="note">
+            <h1>Test Heading</h1>
+            <p>Test Note</p>
+        </div>
+    );
+};
+
+export default Note;
