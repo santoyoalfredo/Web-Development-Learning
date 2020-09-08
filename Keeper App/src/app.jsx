106e08ee204f3aacbe7bad85f8433ecfc406405b
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-09-08 12:46:14 -0400

    Create app.jsx

---
+++ Keeper App/src/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Note from "./components/note";
+
+function App() {
+    return(
+        <div>
+            <Header />
+            <Footer />
+            <Note />
+        </div>
+    )
+};
+
+export default App;
--- Keeper App/src/index.js
+++ Keeper App/src/index.js
@@ -1,5 +1,7 @@
-//1. Create a new React app.
-//2. Create a App.jsx component.
+import ReactDOM from "react-dom";
+import App from "./app";
+
+ReactDOM.render(App, document.getElementById("root"));
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
 //4. Create a Footer.jsx component that renders a <footer> element
