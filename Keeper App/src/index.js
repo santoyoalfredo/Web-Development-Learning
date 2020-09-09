569a9331126dda425af63f87b4e5c478c531490b
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-09-09 12:25:39 -0400

    Refactor using props and map

--- Keeper App/src/index.js
+++ Keeper App/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
+
+
