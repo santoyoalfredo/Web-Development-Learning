199eb258993e7968dc25327dac2c5539a9554f84
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2020-09-11 13:17:38 -0400

    Display notes as they are added

--- Keeper App/src/components/app.jsx
+++ Keeper App/src/components/app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -6,17 +6,29 @@ import CreateArea from "./CreateArea";
 
 function App() {
 
-    let notes = [];
+    const [notes, setNotes] = useState([]);
+
     function addNote(title, content) {
-        notes = [
-            ...notes,
-            { title: title, content: content }]
+
+        setNotes(prevNotes => {
+            return [
+                ...prevNotes,
+                { title: title, content: content }]
+        })
+        console.log(notes);
     }
     return (
         <div>
             <Header />
             <CreateArea onAdd={addNote} />
-            <Note key={1} title="Note title" content="Note content" />
+            {notes.map((note, index) => (
+                <Note
+                    key={index}
+                    index={index}
+                    title={note.title}
+                    content={note.content}
+                />
+            ))}
             <Footer />
         </div>
     );
